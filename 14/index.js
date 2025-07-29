2057015e8261d65e1fc7e7d27e1274e2313ab557
Hadi Tabatabaei <hadi.tabatabaei@divar.ir>
2025-07-29 20:48:51 +0330

    feat: Add session 14

---
+++ 14/index.js
@@ -0,0 +1,84 @@
+// console.log('Index')
+
+// globalThis.setTimeout()
+// console.log(global);
+
+// console.log(window);
+
+// console.log(window)
+
+// چرا این آبجکت توی مرورگر استفاده‌اش خیلی بیشتره از گلوبال توی nodejs
+// window.setTimeout()
+
+// setTimeout()
+
+
+
+// Document Object Model (DOM)
+// Read from DOM (Tag, Attribute, CSS)
+// Write to DOM (Tag, Attribute, CSS)
+
+
+// Tree (درخت)
+// Root‌ Node (ریشه)
+// Node (گره)
+// Node Leaf (برگ)
+// Sibling Node
+
+// DataTypes in DOM:
+// Node
+// Element
+// TextNode
+// AttributeNode
+// HTMLCollection
+// NodeList
+
+// Node: Element (ElementNode)
+// Node: Text (TextNode)
+
+// Web Apis 
+// DOM
+// Fetch
+// XHR (XMLHttpRequest)
+// Geolocation
+// Camera
+// Storage (Cookie, Session Storage, Local Storag, Cache Storage)
+
+console.dir(document)
+// document.location.reload()
+
+document.body // body Element
+
+
+
+
+
+
+
+
+
+
+
+
+// let result = document.getElementById('container')
+// console.log(result)
+// console.dir(result)
+// let result2 = document.getElementsByClassName('foo')
+// console.log(result2);
+// console.dir(result2)
+// Array.from()
+// for(let item of result2) { 
+
+// }
+// document.getElementsByName('bar')
+// document.getElementsByTagName('a')
+// let result3 = document.querySelector("#container")
+// let result4 = document.querySelectorAll('.foo')
+
+let divElement = document.querySelector('#container');
+console.dir(divElement)
+
+// Element (property 1,2,3)
+// HTMLElement (Element + Property5,6)
+// DIVElement (HTMLElement + Property7)
+// AnchorElement (HTMLElement + Property8)
